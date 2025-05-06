a2234b82c70a444c45d7de392334a7bdb99c1bee
Ashutosh Parihar <95736988+Mrgrylls11@users.noreply.github.com>
2025-05-06 16:26:29 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,21 +5,14 @@ FILE=$1
 if [ "$FILE" == "celeba" ]; then
     echo "🔽 Downloading CelebA dataset from Dropbox..."
 
-    # Direct Dropbox download link
     URL="https://www.dropbox.com/scl/fi/75do2onlortal0ltwzoco/celeba.zip?rlkey=2z3i4pqbzzli1pkorm553cq9d&st=r0lx0quu&dl=1"
     ZIP_FILE=./data/celeba.zip
 
-    # Create target folders
     mkdir -p ./data/images
-
-    # Download ZIP file
     wget -N "$URL" -O "$ZIP_FILE"
-
-    # Unzip to data/images
+    
     echo "📦 Unzipping CelebA ZIP..."
     unzip -q "$ZIP_FILE" -d ./data/images
-
-    # Remove ZIP
     rm "$ZIP_FILE"
 
     echo "✅ Done. Data is in ./data/images"
