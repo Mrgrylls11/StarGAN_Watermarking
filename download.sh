be649916b95b2230016fe7d8d186e5bf55ebdd27
Ashutosh Parihar <95736988+Mrgrylls11@users.noreply.github.com>
2025-05-06 16:24:07 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,9 +3,9 @@ if [ $FILE == "celeba" ]; then
     # CelebA images and attribute labels
     URL=https://www.dropbox.com/scl/fi/75do2onlortal0ltwzoco/celeba.zip?rlkey=2z3i4pqbzzli1pkorm553cq9d&st=r0lx0quu&dl=0
     ZIP_FILE=./data/celeba.zip
-    mkdir -p ./data/
+    mkdir -p ./data/images
     wget -N $URL -O $ZIP_FILE
-    unzip $ZIP_FILE -d ./data/
+    unzip $ZIP_FILE -d ./data/images
     rm $ZIP_FILE
 
 else
